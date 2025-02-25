8cc3976fc38f1a2e895d55f88d8a24b0a811caa8
Rishabh5321 <40533251+Rishabh5321@users.noreply.github.com>
2025-02-25 12:44:57 +0530

    Update Miru to version 5.5.10 and refactor packaging

--- flake.nix
+++ flake.nix
@@ -1,41 +1,42 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      lib = pkgs.lib;
-      appimageTools = pkgs.appimageTools;
-      fetchurl = pkgs.fetchurl;
     in
     {
-      packages = {
-        ${system} = {
-          miru = let
-            version = "5.5.6"; # Update this to the correct version of Miru
-            pname = "miru";
-            name = "${pname}-${version}";
+      packages.${system} = {
+        miru = pkgs.appimageTools.wrapType2 rec {
+          name = "miru";
+          pname = "miru";
+          version = "5.5.10";
 
-            src = fetchurl {
-              url = "https://github.com/ThaUnknown/miru/releases/download/v${version}/linux-Miru-${version}.AppImage";
-              hash = "sha256:04fmg7phjxp8axdzvyrliaqnrrzk5lb36x30hn116hgz24dazxn9"; # Replace with the correct sha256 hash
-            };
+          src = pkgs.fetchurl {
+            url = "https://github.com/ThaUnknown/miru/releases/download/v${version}/linux-Miru-${version}.AppImage";
+            hash = "sha256-nLPqEI6u5NNQ/kPbXRWPG0pIwutKNK2J8JeTPN6wHlg=";
+          };
 
-            appimageContents = appimageTools.extractType1 { inherit name src; };
-          in
-          appimageTools.wrapType1 {
-            inherit name src;
-
-            extraInstallCommands = ''
-              mv $out/bin/${name} $out/bin/${pname}
-              install -m 444 -D ${appimageContents}/${pname}.desktop -t $out/share/applications
-              substituteInPlace $out/share/applications/${pname}.desktop \
-                --replace-fail 'Exec=AppRun' 'Exec=${pname}'
-              cp -r ${appimageContents}/usr/share/icons $out/share
+          extraInstallCommands =
+            let
+              contents = pkgs.appimageTools.extract { inherit pname version src; };
+            in
+            ''
+              install -m 444 -D ${contents}/miru.desktop -t $out/share/applications
+              substituteInPlace $out/share/applications/miru.desktop --replace 'Exec=AppRun' 'Exec=${pname}'
+              cp -r ${contents}/usr/share/icons $out/share
             '';
+
+          meta = with pkgs.lib; {
+            description = "Bittorrent streaming software for cats.";
+            homepage = "https://github.com/ThaUnknown/miru";
+            license = licenses.gpl3;
+            maintainers = with maintainers; [ karitham ];
+            platforms = [ "x86_64-linux" ];
+            mainProgram = "miru";
           };
         };
       };
